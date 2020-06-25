e9897ac0a123c1a5cf9649ed2b8f89d6760c8011
ci <ci>
2020-06-25 21:01:47 +0000

    init

---
+++ aliases.js
@@ -0,0 +1,3 @@
+var ALIASES = {};
+ALIASES["cc"] = {};
+ALIASES["gcc_shim"] = {};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cc"] = {"name":"","files":["lib.rs","windows_registry.rs"]};
+sourcesIndex["gcc_shim"] = {"name":"","files":["gcc-shim.rs"]};
+createSourceSidebar();
